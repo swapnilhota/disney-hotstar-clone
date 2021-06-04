e6b894e78662a1ac6f4f56b2a7b76634884fde46
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-04 14:04:10 +0530

    adding styled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,10 +2,24 @@ import styled from 'styled-components';
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Content>
+                Content
+            </Content>
+        </Container>
     );
-}
+};
+
+const Container = styled.section`
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+
+`;
 
 export default Login;
