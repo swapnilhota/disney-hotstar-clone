dc794de9977fca4e88327f91164b1eaff5b88ff2
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-04 14:11:58 +0530

    added images folder

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,17 @@ const Container = styled.section`
 `;
 
 const Content = styled.div`
-
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
 `;
 
 export default Login;
