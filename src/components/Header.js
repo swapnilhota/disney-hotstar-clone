ed609c083fffd23e1e2c852364bf6ca2196bfb99
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-05 14:21:06 +0530

    styling nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,23 @@ import styled from 'styled-components';
 
 const Header = (props) => {
     return (
-        <div>Header</div>
+        <Nav>Header</Nav>
     );
 };
+
+const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: 3;
+`;
 
 export default Header;
