84d1495f7c26b5b90b1c1cb1e695b0f051b2e24e
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-05 13:39:53 +0530

    styling CTA div

--- src/components/Login.js
+++ src/components/Login.js
@@ -50,6 +50,19 @@ const BgImage = styled.div`
 
 const CTA = styled.div`
     margin-bottom: 2vw;
+    max-width: 650px;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 0;
+    align-items: center;
+    text-align: center;
+    margin-right: auto;
+    margin-left: auto;
+    transition-timing-function: ease-out;
+    transition: opacity 0.2s;
+    width: 100%;
 `;
 
 const CTALogoOne = styled.img`
