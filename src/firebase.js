b6ef688f6a7648ce82f6104cdca328f7ca403792
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-07 13:42:46 +0530

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqJoGbEBA8E9M_bUR0l8pHksRiaVxyEgk",
+    authDomain: "disney-hotstar-clone-aab50.firebaseapp.com",
+    projectId: "disney-hotstar-clone-aab50",
+    storageBucket: "disney-hotstar-clone-aab50.appspot.com",
+    messagingSenderId: "1010809083844",
+    appId: "1:1010809083844:web:77935b156fd7438e523691",
+    measurementId: "G-15NH8JTR0K"
+};
