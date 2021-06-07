9618db842594eb8ef3e44a64eacee761d5c0d69f
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-07 13:58:57 +0530

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAqJoGbEBA8E9M_bUR0l8pHksRiaVxyEgk",
     authDomain: "disney-hotstar-clone-aab50.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
     appId: "1:1010809083844:web:77935b156fd7438e523691",
     measurementId: "G-15NH8JTR0K"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export { auth, provider, storage };
+export default db;
