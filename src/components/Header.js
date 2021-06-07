c047efd082cf46d37db5bc4bf477ad34195eae26
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-07 14:36:21 +0530

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,39 @@
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { auth, provider } from "../firebase";
+import {
+    selectUserName,
+    selectUserPhoto,
+    setUserLoginDetails
+} from '../features/user/userSlice';
 
 const Header = (props) => {
+
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const username = useSelector(selectUserName);
+    const userPhoto = useSelector(selectUserPhoto);
 
     const handleAuth = () => {
         auth.signInWithPopup(provider)
             .then((result) => {
-                console.log(result);
+                setUser(result.user);
             })
             .catch((error) => {
                 alert(error.message);
             })
     };
+
+    const setUser = (user) => {
+        dispatch(
+            setUserLoginDetails({
+                name: user.displayName,
+                email: user.email,
+                photo: user.PhotoURL
+            })
+        )
+    }
 
     return (
         <Nav>
