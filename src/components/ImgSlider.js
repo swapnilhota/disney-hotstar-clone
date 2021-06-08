b44d130c9378047aae04e292960a4da5a2697861
Swapnil Hota <swapnilhota99@gmail.com>
2021-06-08 14:52:44 +0530

    added slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,8 +1,30 @@
 import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 const ImgSlider = (props) => {
+
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+    }
+
     return (
-        <div>Img Slider</div>
+        <div>
+            <Slider {...settings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+            </Slider>
+        </div>
     );
 }
 
